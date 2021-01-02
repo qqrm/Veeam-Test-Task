26338c6a6876c98bd747c0674fe2075caa182d45
Алексей Кот <inspctr.cat@gmail.com>
2021-01-02 09:32:11 +0300

    init

---
+++ veeam_test/veeam_test.cpp
@@ -0,0 +1,12 @@
+﻿// Veeam Test Task.cpp : Defines the entry point for the application.
+//
+
+#include "veeam_test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
